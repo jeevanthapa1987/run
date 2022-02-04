a1dddde9b876e5f78eb91c3713e77330c7fc3117
Jeevan Thapa <jeevanthapa1987@yahoo.com>
2022-02-04 12:43:37 -0500

    portfolio

--- this.py
+++ this.py
@@ -5,3 +5,11 @@ https://static.fullstack-bootcamp.com/module-1/privacy-policy-partial.html
 https://static.fullstack-bootcamp.com/module-2/flipped-hero-image.jpg
 
   background-image: url("https://static.fullstack-bootcamp.com/module-1/hero-bg.jpg");
+
+  header p{
+        
+        width: 50%;
+        vertical-align: top;
+        margin: -400px auto -300px auto;
+        text-align:center;
+    }
